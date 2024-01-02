549e8093c41464d9ab40f4a6d42be2998ee89141
Hilal Mufti <hmufti@cs.washington.edu>
2024-01-02 02:38:47 -0800

    refactor

--- donut.rkt
+++ donut.rkt
@@ -1,13 +1,7 @@
 #lang racket
 
-(define (donut-2d x y [radius 0.4] [thickness 0.3])
-  (- (abs (- (sqrt (+ (expt x 2) (expt y 2))) radius)) (/ thickness 2)))
-
-(define (sphere x y z [radius 0.4])
-  (- (sqrt (+ (expt x 2) (expt y 2) (expt z 2))) radius))
-
-(define (circle x y [radius 0.4])
-  (- (sqrt (+ (expt x 2) (expt y 2))) radius))
+(define (time)
+  (/ (current-inexact-milliseconds) 1000.0))
 
 (define (donut x y z [radius 0.4] [thickness 0.3])
   (let* ([xy-d (- (sqrt (+ (sqr x) (sqr y))) radius)]
@@ -15,27 +9,40 @@
     (- d (/ thickness 2))))
 
 (define (sample x y)
-  (define (go z i steps)
-    (if (>= i steps) #\space
-    (let* ([theta (* 2 (/ (current-inexact-milliseconds) 1000.0))]
-           [t-x (- (* x (cos theta)) (* z (sin theta)))]
-           [t-z (+ (* x (sin theta)) (* z (cos theta)))]
-           [d (donut t-x y t-z)])
-      (cond [(<= d 0.01) #\#]
-            [else (go (+ z d) (add1 i) steps)]))))
-  (go -10 0 30))
+  (let loop ([z -10] [steps 30])
+    (cond [(zero? steps) #\space]
+          [else (let* ([theta (* 2 (time))]
+                       [t-x (- (* x (cos theta)) (* z (sin theta)))]
+                       [t-z (+ (* x (sin theta)) (* z (cos theta)))]
+                       [d (donut t-x y t-z)])
+                  (cond [(<= d 0.01) #\#]
+                        [else (loop (sub1 steps) (+ z d))]))])))
 
 (define (render-frame)
   (define frame-chars
     (for*/list ([y 20]
                 [x 81])
-        (if (= x 80) #\newline 
-        (let ([x-remap (sub1 (* (/ x 80) 2))]
-              [y-remap (* (sub1 (* (/ y 20) 2)) (* 2 (/ 20 80)))])
-          (sample x-remap y-remap)))))
+      (if (= x 80) #\newline
+          (let ([x-remap (sub1 (* (/ x 80) 2))]
+                [y-remap (* (sub1 (* (/ y 20) 2)) (* 2 (/ 20 80)))])
+            (sample x-remap y-remap)))))
 
-    (display "\033[2J")
-    (display (list->string frame-chars))
-    (sleep (/ 1 60))
-    (render-frame))
+  (display "\033[2J")
+  (display (list->string frame-chars))
+  (sleep (/ 1 60))
+  (render-frame))
+
 (render-frame)
+
+; (let render ()
+;   (let ([frame-chars (for*/list ([y 20]
+;                                  [x 81])
+;                        (if (= x 80) #\newline
+;                            (let ([x-remap (sub1 (* (/ x 80) 2))]
+;                                  [y-remap (* (sub1 (* (/ y 20) 2)) 
+;                                              (* 2 (/ 20 80)))])
+;                              (sample x-remap y-remap))))])
+;     (display "\033[2J")
+;     (display (list->string frame-chars))
+;     (sleep (/ 1 60)))
+;   (render))
