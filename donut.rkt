50460181c9ebbd840fb356e76460071d63a8fe63
Hilal Mufti <hmufti@cs.washington.edu>
2024-01-02 02:23:27 -0800

    +3d spinning donut

--- donut.rkt
+++ donut.rkt
@@ -10,32 +10,32 @@
   (- (sqrt (+ (expt x 2) (expt y 2))) radius))
 
 (define (donut x y z [radius 0.4] [thickness 0.3])
-  (let* ([xy-d (- (sqrt (+ (expt x 2) (expt y 2))) radius)]
-         [d (sqrt (+ (expt xy-d 2) (expt z 2)))])
+  (let* ([xy-d (- (sqrt (+ (sqr x) (sqr y))) radius)]
+         [d (sqrt (+ (sqr xy-d) (sqr z)))])
     (- d (/ thickness 2))))
 
 (define (sample x y)
   (define (go z i steps)
     (if (>= i steps) #\space
-    (let ([d (donut x y z)])
+    (let* ([theta (* 2 (/ (current-inexact-milliseconds) 1000.0))]
+           [t-x (- (* x (cos theta)) (* z (sin theta)))]
+           [t-z (+ (* x (sin theta)) (* z (cos theta)))]
+           [d (donut t-x y t-z)])
       (cond [(<= d 0.01) #\#]
             [else (go (+ z d) (add1 i) steps)]))))
   (go -10 0 30))
 
 (define (render-frame)
   (define frame-chars
-    (flatten 
-    (for/list ([y 20])
-      (append
-      (for/list ([x 80])
+    (for*/list ([y 20]
+                [x 81])
+        (if (= x 80) #\newline 
         (let ([x-remap (sub1 (* (/ x 80) 2))]
               [y-remap (* (sub1 (* (/ y 20) 2)) (* 2 (/ 20 80)))])
-          (sample x-remap y-remap)))
-      #\newline))))
+          (sample x-remap y-remap)))))
 
     (display "\033[2J")
     (display (list->string frame-chars))
-    (sleep (/ 1 30))
+    (sleep (/ 1 60))
     (render-frame))
-
 (render-frame)
