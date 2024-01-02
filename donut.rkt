ced98c80a2ae413c99dcfccebd331fc3c25d1986
Hilal Mufti <hmufti@cs.washington.edu>
2024-01-02 01:58:06 -0800

    +donut

--- donut.rkt
+++ donut.rkt
@@ -9,10 +9,15 @@
 (define (circle x y [radius 0.4])
   (- (sqrt (+ (expt x 2) (expt y 2))) radius))
 
-(define (sample x y [radius 0.4])
+(define (donut x y z [radius 0.4] [thickness 0.3])
+  (let* ([xy-d (- (sqrt (+ (expt x 2) (expt y 2))) radius)]
+         [d (sqrt (+ (expt xy-d 2) (expt z 2)))])
+    (- d (/ thickness 2))))
+
+(define (sample x y)
   (define (go z i steps)
     (if (>= i steps) #\space
-    (let ([d (sphere x y z)])
+    (let ([d (donut x y z)])
       (cond [(<= d 0.01) #\#]
             [else (go (+ z d) (add1 i) steps)]))))
   (go -10 0 30))
@@ -27,6 +32,7 @@
               [y-remap (* (sub1 (* (/ y 20) 2)) (* 2 (/ 20 80)))])
           (sample x-remap y-remap)))
       #\newline))))
+
     (display "\033[2J")
     (display (list->string frame-chars))
     (sleep (/ 1 30))
