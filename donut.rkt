86d82ad52b9c8696244ba7409c47ab70397dddb8
Hilal Mufti <hmufti@cs.washington.edu>
2024-01-03 00:08:42 -0800

    +spinning donut

--- donut.rkt
+++ donut.rkt
@@ -1,48 +1,31 @@
 #lang racket
 
-(define (time)
-  (/ (current-inexact-milliseconds) 1000.0))
-
-(define (donut x y z [radius 0.4] [thickness 0.3])
-  (let* ([xy-d (- (sqrt (+ (sqr x) (sqr y))) radius)]
+(define (donut x y z [rad 0.4] [thick 0.3])
+  (let* ([xy-d (- (sqrt (+ (sqr x) (sqr y))) rad)]
          [d (sqrt (+ (sqr xy-d) (sqr z)))])
-    (- d (/ thickness 2))))
+    (- d (/ thick 2))))
 
 (define (sample x y)
-  (let loop ([z -10] [steps 30])
-    (cond [(zero? steps) #\space]
-          [else (let* ([theta (* 2 (time))]
-                       [t-x (- (* x (cos theta)) (* z (sin theta)))]
-                       [t-z (+ (* x (sin theta)) (* z (cos theta)))]
-                       [d (donut t-x y t-z)])
-                  (cond [(<= d 0.01) #\#]
-                        [else (loop (sub1 steps) (+ z d))]))])))
+  (define (go z i steps)
+    (if (>= i steps) #\space
+    (let* ([theta (* 2 (/ (current-inexact-milliseconds) 1000.0))]
+           [t-x (- (* x (cos theta)) (* z (sin theta)))]
+           [t-z (+ (* x (sin theta)) (* z (cos theta)))]
+           [d (donut t-x y t-z)])
+      (cond [(<= d 0.01) #\#]
+            [else (go (+ z d) (add1 i) steps)]))))
+  (go -10 0 30))
 
-(define (render-frame)
-  (define frame-chars
+(let loop ()
+  (define frame
     (for*/list ([y 20]
                 [x 81])
       (if (= x 80) #\newline
-          (let ([x-remap (sub1 (* (/ x 80) 2))]
-                [y-remap (* (sub1 (* (/ y 20) 2)) (* 2 (/ 20 80)))])
-            (sample x-remap y-remap)))))
-
+      (let ([x-remap (sub1 (* (/ x 80) 2))]
+            [y-remap (* (sub1 (* (/ y 20) 2)) (* 2 (/ 20 80)))])
+        (sample x-remap y-remap)))))
+  
   (display "\033[2J")
-  (display (list->string frame-chars))
+  (display (list->string frame))
   (sleep (/ 1 60))
-  (render-frame))
-
-(render-frame)
-
-; (let render ()
-;   (let ([frame-chars (for*/list ([y 20]
-;                                  [x 81])
-;                        (if (= x 80) #\newline
-;                            (let ([x-remap (sub1 (* (/ x 80) 2))]
-;                                  [y-remap (* (sub1 (* (/ y 20) 2)) 
-;                                              (* 2 (/ 20 80)))])
-;                              (sample x-remap y-remap))))])
-;     (display "\033[2J")
-;     (display (list->string frame-chars))
-;     (sleep (/ 1 60)))
-;   (render))
+  (loop))
